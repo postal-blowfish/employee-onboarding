8252d91843029817c093e1112c0140bf4765ffb1
Kairo97 <109006341+kairo97@users.noreply.github.com>
2024-02-20 13:32:15 -0800

    got all videos loading and unloading properly

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const vidBtn1 = document.querySelector('#vid-title1')
 let vidBtn2 = document.querySelector('#vid-title2')
-let vidbtn3 = document.querySelector('#vid-title3')
+let vidBtn3 = document.querySelector('#vid-title3')
 let vidBtn4 = document.querySelector('#vid-title4')
 let vidBtn5 = document.querySelector('#vid-title5')
 let vidBtn6 = document.querySelector('#vid-title6')
@@ -39,12 +39,120 @@ function toggle1() {
      if (vidBox1.style.display && vid1.style.display == "none") {
          vidBox1.style.display = "block";
          vid1.style.display = "block";
-         console.log('hello')
         } else {
             vidBox1.style.display = "none";
             vid1.style.display ="none";
         }
     }
+    function toggle2() {
 
+        if (vidBox2.style.display && vid2.style.display == "none") {
+            vidBox2.style.display = "block";
+            vid2.style.display = "block";
+           } else {
+               vidBox2.style.display = "none";
+               vid2.style.display ="none";
+           }
+       }
+       function toggle3() {
+
+        if (vidBox3.style.display && vid3.style.display == "none") {
+            vidBox3.style.display = "block";
+            vid3.style.display = "block";
+           } else {
+               vidBox3.style.display = "none";
+               vid3.style.display ="none";
+           }
+       }
+       function toggle4() {
+
+        if (vidBox4.style.display && vid4.style.display == "none") {
+            vidBox4.style.display = "block";
+            vid4.style.display = "block";
+           } else {
+               vidBox4.style.display = "none";
+               vid4.style.display ="none";
+           }
+       }
+       function toggle5() {
+
+        if (vidBox5.style.display && vid5.style.display == "none") {
+            vidBox5.style.display = "block";
+            vid5.style.display = "block";
+           } else {
+               vidBox5.style.display = "none";
+               vid5.style.display ="none";
+           }
+       }
+       function toggle6() {
+
+        if (vidBox6.style.display && vid6.style.display == "none") {
+            vidBox6.style.display = "block";
+            vid6.style.display = "block";
+           } else {
+               vidBox6.style.display = "none";
+               vid6.style.display ="none";
+           }
+       }
+       function toggle7() {
+
+        if (vidBox7.style.display && vid7.style.display == "none") {
+            vidBox7.style.display = "block";
+            vid7.style.display = "block";
+           } else {
+               vidBox7.style.display = "none";
+               vid7.style.display ="none";
+           }
+       }
+       function toggle8() {
+
+        if (vidBox8.style.display && vid8.style.display == "none") {
+            vidBox8.style.display = "block";
+            vid8.style.display = "block";
+           } else {
+               vidBox8.style.display = "none";
+               vid8.style.display ="none";
+           }
+        }
+        function toggle9() {
+
+            if (vidBox9.style.display && vid9.style.display == "none") {
+                vidBox9.style.display = "block";
+                vid9.style.display = "block";
+               } else {
+                   vidBox9.style.display = "none";
+                   vid9.style.display ="none";
+               }
+           }
+           function toggle10() {
+
+            if (vidBox10.style.display && vid10.style.display == "none") {
+                vidBox10.style.display = "block";
+                vid10.style.display = "block";
+               } else {
+                   vidBox10.style.display = "none";
+                   vid10.style.display ="none";
+               }
+           }
+           function toggle11() {
+
+            if (vidBox11.style.display && vid11.style.display == "none") {
+                vidBox11.style.display = "block";
+                vid11.style.display = "block";
+               } else {
+                   vidBox11.style.display = "none";
+                   vid11.style.display ="none";
+               }
+           };
 
 vidBtn1.addEventListener('click', toggle1);
+vidBtn2.addEventListener('click', toggle2);
+vidBtn3.addEventListener('click', toggle3);
+vidBtn4.addEventListener('click', toggle4);
+vidBtn5.addEventListener('click', toggle5);
+vidBtn6.addEventListener('click', toggle6);
+vidBtn7.addEventListener('click', toggle7);
+vidBtn8.addEventListener('click', toggle8);
+vidBtn9.addEventListener('click', toggle9);
+vidBtn10.addEventListener('click', toggle10);
+vidBtn11.addEventListener('click', toggle11);
